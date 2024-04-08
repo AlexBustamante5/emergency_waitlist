4671cfb3434a778259d76b972cb5bae41817db3b
Alex Bustamante <90732151+AlexBustamante5@users.noreply.github.com>
2024-04-08 18:47:42 -0400

    created signup files

---
+++ signup.php
@@ -0,0 +1,21 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Signup Page</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Sign up  </h1>
+    <form action="signup.php" method="POST">
+        <label for="username">Username:</label>
+        <input type="text" id="username" name="username" required>
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" required>
+        <button type="submit">Sign Up</button>
+    </form>
+</body>
+</html>
+
